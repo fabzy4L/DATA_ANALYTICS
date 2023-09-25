422d27d3fec2473e363cdb7144db8e6da3a0feb4
f4l <63645254+fabzy4L@users.noreply.github.com>
2023-09-24 18:10:58 -0600

    092423

--- Pandas/CSV_IMPORT.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-qPCR = pd.read_csv('/Users/f4L/Library/Containers/com.microsoft.Excel/Data/Desktop/RStudioDataAnalysis/GENE EXPRESSION NICOTINE DATA ANALYSIS/qPCR all values - qPCR.csv')
-print(qPCR)
